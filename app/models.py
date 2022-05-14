2f9a58c3fafbb29c2d74a4043fbb166a485630a5
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-05-14 09:03:33 +0300

    adding bootstrap link

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from turtle import title
+# from turtle import title
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY=os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:jaysafu@localhost/pitcheslist'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:jay123@localhost/blogpost'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
 
     #  email configurations
@@ -26,19 +26,20 @@ class ProdConfig(Config):
     #     uri = uri.replace("postgres://", "postgresql://", 1)
 
     #     SQLALCHEMY_DATABASE_URI=uri
-     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-
+    #  SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+  pass
 class TestConfig(Config):
-      SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:jaysafu@localhost/pitcheslist_test'
-  
+    #   SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:jaysafu@localhost/pitcheslist_test'
+  pass
+
 class DevConfig(Config):
     '''
     Development  configuration child class
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:jaysafu@localhost/pitcheslist'
-
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:jay123@localhost/blogpost'
+ 
 DEBUG = True
 
 config_options = {
