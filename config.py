d1c8aca14d2cf962e7614fa96e03714c3971d0fe
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-05-15 12:54:31 +0300

    Heroku deployment

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 class Config:
     QUOTE_API_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
     SECRET_KEY=os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgres://mwtvnlerdottey:f8a17940e5c7983fd4ae2999dfa5939498ffbc0cdd0ea2f63620d54647c87807@ec2-54-165-184-219.compute-1.amazonaws.com:5432/dcmp8l9rgi8chs'
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:jay123@localhost/blogpost'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
 
@@ -22,16 +23,16 @@ class Config:
         pass
 
 class ProdConfig(Config):
-    #  uri = os.getenv("DATABASE_URL")  # or other relevant config var
-    #  if uri.startswith("postgres://"):
-    #     uri = uri.replace("postgres://", "postgresql://", 1)
+     uri = os.getenv("DATABASE_URL")  
+     if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
 
-    #     SQLALCHEMY_DATABASE_URI=uri
-    #  SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-  pass
+        SQLALCHEMY_DATABASE_URI=uri
+  
+  
 class TestConfig(Config):
-    #   SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:jaysafu@localhost/pitcheslist_test'
-  pass
+      SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:jaysafu@localhost/blogpost_test'
+  
 
 class DevConfig(Config):
     '''
